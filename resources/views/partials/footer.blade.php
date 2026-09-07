07f4c6a4338bba5db77e1d8846bee2a9547a313b
AndreaDistefanoDev7 <andre.distefano99@gmail.com>
2026-09-07 14:30:44 +0200

    Enhance footer layout with improved styling and additional links

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,15 @@
-<footer>
+<footer class="text-white-50">
+    <div class="w-100 overflow-hidden"
+    style="background-image: url('{{ Vite::asset('resources/img/footer-bg.jpg') }}'); 
+                background-size: cover; 
+                background-repeat: no-repeat; 
+                padding-top: 3rem; 
+                padding-bottom: 3rem;">
     <div class="container">
         <div class="row">
-            <div class="col">
-                <p>DC COMICS</p>
-                <ul>
+            <div class="col col-md-2 col-6 mb-4 mb-md-0">
+                <p class="text-white text-uppercase font-weight-bold mb-3 small" style="letter-spacing: 0.5px";>DC COMICS</p>
+                <ul class="list-unstyled lh-sm small mb-4">
                     <li>Characters</li>
                     <li>Comics</li>
                     <li>Movies</li>
@@ -13,11 +19,16 @@
                     <li>News</li>
 
                 </ul>
+                    <p class="text-white text-uppercase font-weight-bold mb-3 small" style="letter-spacing: 0.5px;">SHOP</p>
+                <ul class="list-unstyled lh-sm small m-0">>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collecltibles</li>
+                </ul>
             </div>
-            <div class="col">
+            <div class="col-md-2 col-6 mb-4 mb-md-0" ">
                 
-                <p>DC</p>
-                <ul>
+                <p class="text-white text-uppercase font-weight-bold mb-3 small" style="letter-spacing: 0.5px;">DC</p>
+                <ul class="list-unstyled lh-sm small m-0">
                     <li>Terms Of Use</li>
                     <li>Privacy Policy</li>
                     <li>Ad Choices</li>
@@ -32,9 +43,9 @@
 
                 </ul>
             </div> 
-            <div class="col">
-                <p>SITES</p>
-                <ul>
+            <div class="col-md-2 col-6">
+                <p class="text-white text-uppercase font-weight-bold mb-3 small" style="letter-spacing: 0.5px;">SITES</p>
+                <ul class="list-unstyled lh-sm small m-0">
                     <li>DC</li>
                     <li>MAD Magazine</li>
                     <li>DC Kids</li>
@@ -42,13 +53,15 @@
                     <li>DC Power Visa</li>
                 </ul>
             </div>
-            <div class="col">
-                <p>SHOP</p>
-                <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collecltibles</li>
-                </ul>
-            </div>
+        
+        <div class="col-md-6 d-none d-md-block position-relative">
+                    <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" 
+                        alt="DC Logo Gigante" 
+                        style="position: absolute; right: 0; top: -140px; height: 480px; transform: rotate(-10deg); opacity: 0.9; object-fit: contain;">
+                </div>
+            
 
     </div>
+    </div>
+
 </footer>
